4a49cbcfb36673db68b1b51bc2c2b223a7b765fd
Edward Garson <egarson@gmail.com>
2011-08-23 12:31:04 -0400

    refactored to class

--- core.py
+++ core.py
@@ -4,5 +4,10 @@ SUITS = ['Diamonds', 'Clubs', 'Spades', 'Hearts']
 RANKS = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
 
 def test_define_deck():
-    cards = [(rank,suit) for rank in RANKS for suit in SUITS]
-    eq_(52, len(set(cards))) # kill 2 birds: assert len && ensure no dupes
+    deck = Deck()
+    eq_(52, len(set(deck.cards))) # kill 2 birds: assert len && ensure no dupes
+
+class Deck():
+    def __init__(self):
+        self.cards = [(rank,suit) for rank in RANKS for suit in SUITS]
+
