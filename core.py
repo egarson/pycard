8366ce9ed8baa8b58f54c544f8a061f5ef80b3ec
Edward Garson <egarson@gmail.com>
2011-08-24 14:06:13 -0400

    refactored to more terse and comprehensive tests for rank and suit

--- core.py
+++ core.py
@@ -15,20 +15,18 @@ def test_define_deck():
     deck = Deck()
     eq_(52, len(set(deck.cards))) # usage of set ensures no dupes
     eq_(52, len(deck)) # tests __len__() - not the same as above
-    eq_(13, len(filter(lambda card: card.suit == DIAMONDS, deck.cards))) # sanity checks
-    eq_(13, len(filter(lambda card: card.suit == CLUBS, deck.cards)))
-    eq_(13, len(filter(lambda card: card.suit == SPADES, deck.cards)))
-    eq_(13, len(filter(lambda card: card.suit == HEARTS, deck.cards)))
+    # check that we have 13 per suit & 4 per rank
+    map(lambda suit: eq_(13, len(filter(lambda card: card.suit == suit, deck.cards))), SUITS)
+    map(lambda rank: eq_(4, len(filter(lambda card: card.rank == rank, deck.cards))), RANKS)
 
 def test_deck_order():
     deck = Deck()
     eq_('Ace of Clubs', str(deck.cards[0])) # lowest card tests
-    eq_((0,1), (deck.cards[0].suit, deck.cards[0].rank))
-    eq_((0,1), (deck.first_card.suit, deck.first_card.rank))
+    eq_((0,1), (deck.cards[0].suit, deck.cards[0].rank)) # yuk
+    eq_((0,1), (deck.first_card.suit, deck.first_card.rank)) # ok, better
     eq_('King of Spades', str(deck.cards[len(deck)-1])) # highest card tests
     eq_((3,13), (deck.cards[len(deck)-1].suit, deck.cards[len(deck)-1].rank))
     eq_((3,13), (deck.last_card.suit, deck.last_card.rank))
-
 
 class Card():
     def __init__(self,suit,rank):
