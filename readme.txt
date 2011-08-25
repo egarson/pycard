4a546aaf60b1ffd525bc8d4e9fa321ff8b740999
Edward Garson <egarson@gmail.com>
2011-08-25 12:37:28 -0400

    split tests out of core

---
+++ readme.txt
@@ -0,0 +1,5 @@
+utf-8 ok
+aces_high method
+would have prefered adding a method to e.g. SPADES (a scalar) but python won't let me ('immutable type')
+core has two distinct interfaces, one for computers (i.e. processing), and one for humans (i.e. reasoning): ints for the former, chars for the latter
+Yes, there is some overlap in *_chars decls, but it is coincidental, and besides: I'm lazy
---
+++ test_core.py
@@ -0,0 +1,19 @@
+from core import *
+
+def test_define_deck():
+    deck = Deck()
+    eq_(52, len(set(deck.cards))) # usage of set ensures no dupes
+    eq_(52, len(deck)) # tests __len__() - not the same as above
+    # check that we have 13 per suit & 4 per rank
+    map(lambda suit: eq_(13, len(filter(lambda card: card.suit == suit, deck.cards))), SUITS)
+    map(lambda rank: eq_(4, len(filter(lambda card: card.rank == rank, deck.cards))), RANKS)
+
+def test_deck_order():
+    deck = Deck()
+    eq_('Ace of Clubs', str(deck.cards[0])) # lowest card tests
+    eq_((0,1), (deck.cards[0].suit, deck.cards[0].rank)) # yuk
+    eq_((0,1), (deck.first_card.suit, deck.first_card.rank)) # ok, better
+    eq_('King of Spades', str(deck.cards[len(deck)-1])) # highest card tests
+    eq_((3,13), (deck.cards[len(deck)-1].suit, deck.cards[len(deck)-1].rank))
+    eq_((3,13), (deck.last_card.suit, deck.last_card.rank))
+
